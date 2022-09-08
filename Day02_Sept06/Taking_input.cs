a6417f12a3f59852e0a3e0e1d74c4d1440dbc04e
ShubhamA279 <112865907+ShubhamA279@users.noreply.github.com>
2022-09-08 22:15:06 +0530

    Add files via upload

---
+++ Day02_Sept06/Taking_input.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_02_Sept06
+{
+    internal class Taking_input
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.WriteLine("enter any two numbers");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = n1 + n2;
+            int mul = n1 * n2;
+            int div = n1 / n2;
+            int sub = n1 - n2;
+            int mod = n1 % n2;
+
+            Console.WriteLine($"addition is : {sum}");
+            Console.WriteLine($"multiplication is : {mul}");
+            Console.WriteLine($"division is : {div}");
+            Console.WriteLine($"subtraction is : {sub}");
+            Console.WriteLine($"remainder is : {mod}");
+        }
+    }
+}
+
