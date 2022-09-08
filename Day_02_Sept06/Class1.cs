758c412d10c8d73f620b514fe4e78a724b7e1db4
ShubhamA279 <112865907+ShubhamA279@users.noreply.github.com>
2022-09-08 21:08:32 +0530

    Add files via upload

---
+++ Day_02_Sept06/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_02_Sept06
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            // int n1 = 15, n2 = 5,n3=21;
+
+            // //Console.WriteLine(n1>n2);
+            // //Console.WriteLine(n1>=n2);
+            // //Console.WriteLine(n1<n2);
+            // //Console.WriteLine(n1<=n2);
+            // //Console.WriteLine(n1==n2);
+            // //Console.WriteLine(n1!=n2);
+
+            //// int ans = Convert.ToInt32(n1 > n2 && n1 > n3);
+            // bool ans1 = (n1 >= n2 || n1 < n3);
+
+            // Console.WriteLine(ans1); 
+
+            String str = "local";
+            int num = 18;
+
+            if(str== "local" && num== 18)
+            {
+                Console.WriteLine("vote");
+            }
+            else
+                Console.WriteLine("cant");
+        }
+    }
+}
