c89d1a19376251861c40760414df12a8fcb1f1fa
ShubhamA279 <112865907+ShubhamA279@users.noreply.github.com>
2022-09-08 21:06:51 +0530

    Add files via upload

---
+++ Day02_Sept06/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day04_Sept08
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for(i=1;i<=50;i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
