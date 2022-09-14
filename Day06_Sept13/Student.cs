f79274e8a1ae79eed1caf7edcc5da1575721aed8
ShubhamA279 <112865907+ShubhamA279@users.noreply.github.com>
2022-09-15 01:58:25 +0530

    Add files via upload

---
+++ Day06_Sept13/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day06_Sept13
+{
+    public class Student
+    {
+        int roll_no;
+        string name,city;
+        int age;
+        string branch;
+
+
+
+
+        public void AcceptValue()
+        {
+            roll_no = 27;
+            name = "Amit";
+            city = "Ratnagiri";
+            age = 24;
+            branch = "Electrical";
+        }
+
+        public string DisplayValue()
+
+        {
+            return $"Roll No is : {roll_no} Name is :{name} City is :{city} Age is : {age} Branch :{branch}" ;
+        //    return $"{name}";
+        //    return $"{city}";
+        //    return $"{age}";
+        //    return $"{branch}";
+        }
+    }
+}
