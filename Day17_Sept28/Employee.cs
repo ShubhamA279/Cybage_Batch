93f68ff86e343b1b259825331fbd5d150512e3c9
ShubhamA279 <112865907+ShubhamA279@users.noreply.github.com>
2022-10-03 01:03:46 +0530

    Add files via upload

---
+++ Day17_Sept28/Employee.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17_Sept28
+{
+    public class Emp
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+
+        public List<Lones> Lones1 = new List<Lones>();
+    }
+    public class Department
+    {
+        public int Did { get; set; }
+        public string Dname { get; set; }
+
+        public List<Emp> Employees = new List<Emp>();
+    }
+    public class Lones
+    {
+        public string LoneName { get; set; }
+    }
+    public class Account
+    {
+        public string AccName { get; set; }
+
+        public List<Department> Departments = new List<Department>();
+    }
+    public class Program1
+    {
+        static void Main(string[] args)
+        {
+            List<Account> AccName = new List<Account>()
+            {
+                new Account
+                {
+                    AccName = "Account Type",
+
+                    Departments =
+                    {
+                        new Department
+                        {
+                            Did = 101,
+                            Dname = "HR",
+                            Employees =
+                            {
+                                new Emp
+                                {
+                                    EmpId = 1,
+                                    EmpName = "User1",
+                                    Lones1 =
+                                    {
+                                        new Lones
+                                        {
+                                            LoneName = "Car",
+                                        }
+                                    }
+                                },
+                                new Emp
+                                {
+                                    EmpId = 2,
+                                    EmpName = "User2",
+                                    Lones1 =
+                                    {
+                                        new Lones
+                                        {
+                                            LoneName = "HomeLone",
+                                        }
+                                    }
+                                },
+
+                            }
+                        },
+
+                        //new Department
+                        //{
+                        //    Did=101,Dname="Development",
+                        //    Employees={
+                        //        new Emp{EmpId=3,EmpName="User3"},
+                        //        new Emp{EmpId=4,EmpName="User4"},
+                        //        new Emp{EmpId=5,EmpName="User5"},
+                        //    }
+                        //},
+
+                    }
+
+
+
+                }
+
+            
+            };
+            Console.WriteLine("Account type");
+            foreach (var v in AccName)
+            {
+                Console.WriteLine($"{v.AccName}");
+                foreach (Department d in v.Departments)
+                {
+                    Console.WriteLine($"\t{d.Did} {d.Dname}");
+                    foreach (Emp e in d.Employees)
+                    {
+                        Console.WriteLine($"\t\t{e.EmpId}   {e.EmpName}");
+                        foreach (Lones l in e.Lones1)
+                        {
+                            Console.WriteLine($"\t\t\t{l.LoneName}");
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+
+}
+
+        
+    
+
---
+++ Day17_Sept28/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17_Sept28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "I am Shubham";
+            string str2 = "Adding Example for string";
+            int num = 27;
+            Console.WriteLine(str1.Length);
+            Console.WriteLine(str2.Length);
+
+            string str3 = str1.Substring(5);
+             string str10= str1.Substring(5,7);
+            Console.WriteLine(str3);
+            Console.WriteLine(str10);
+            
+            string str4 = String.Concat(str1, " ", str2," ",num.ToString());
+            Console.WriteLine(str4);
+            Console.WriteLine("---------");
+            char[] chars = str1.ToCharArray(5, 7);
+            foreach (var item in chars)
+            {
+                Console.WriteLine(item);
+            }
+
+            string str5 = "Hello";
+            Console.WriteLine(str5.ToLower());
+            Console.WriteLine(str5.ToUpper());
+            
+            bool result = str5.Contains("hello");
+            Console.WriteLine(result);
+
+            string str6 = "hello Everyone";
+            string str7 = " Good Evening";
+            string str8 = str6;
+
+            Console.WriteLine("---------");
+            Console.WriteLine(str6.GetHashCode());
+            Console.WriteLine(str8.GetHashCode());
+
+
+            bool result1 = str6.Equals(str8);
+            bool result2 = str6.Equals(str7);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+
+
+
+
+
+
+
+
+        }
+    }
+}
