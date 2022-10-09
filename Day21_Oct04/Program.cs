95dcdf683a03ed3534206e2006894a6813d78e93
ShubhamA279 <112865907+ShubhamA279@users.noreply.github.com>
2022-10-09 14:14:23 +0530

    Add files via upload

---
+++ Day21_Oct04/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21_Oct04
+{
+    public delegate string Mydelegate(string s1);
+    public class User
+    {
+        public string AcceptName(string str1)
+        {
+           return str1.ToUpper();
+            //return str1;
+        }
+        public string acceptname(string str1)
+        {
+           return  str1.ToLower();
+            
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User u1 = new User();
+
+            Mydelegate mydel = new Mydelegate(u1.AcceptName);
+
+            mydel += new Mydelegate(u1.acceptname);
+
+            Delegate[] list = mydel.GetInvocationList();
+            foreach (Delegate d in list)
+            {
+               
+                Console.WriteLine(d.DynamicInvoke("Amit"));
+            }
+
+
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
